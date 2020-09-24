a0c915a4d2398baa2300e9a9bfeabc2d3c9caa7b
bkorn68 <bkorn@gmx.de>
2020-09-24 16:15:12 +0200

    todo-app add todos

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <Todos v-bind:todos="todos"/>
+    <AddTodo v-on:add-todo="addTodo"/>
   </div>
 </template>
 <script>
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -41,6 +44,11 @@ export default {
       ],
     }
   },
+  methods: {
+    addTodo(newTodoObj) {
+      this.todos = [...this.todos, newTodoObj];
+    }
+  }
 }
 </script>
 <style>
