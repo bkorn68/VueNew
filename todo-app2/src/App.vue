d603a0587ebd9c8b663e07723985afc1ec6fd466
bkorn68 <bkorn@gmx.de>
2020-09-24 14:25:57 +0200

    todo-app2 by my own

---
+++ todo-app2/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+
+   <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+
+<script>
+import Todos from "./components/Todos"
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+ todos: [
+
+      {
+        title: "Medikamente kaufen",
+        erledigt: false
+      },
+      {
+        title: "Vue-Dokumentation lesen",
+        erledigt: false
+      },
+      {
+        title: "Film schauen",
+        erledigt: false
+      },
+      {
+        title: "Anja anrufen",
+        erledigt: false
+      },
+      {
+        title: "Ines anrufen",
+        erledigt: false
+      }
+    ]    }
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo-app2/src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Meine Aufgabeliste</h2>
+    <ul>
+      <li v-bind:key="todo.id" v-for="todo in todos">
+        <Todo v-bind:todo="todo" />
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import Todo from "./Todo";
+export default {
+    name: "Todos",
+    components: {
+        Todo
+    },
+    props: [
+        "todos"
+    ]
+
+}
+</script>
