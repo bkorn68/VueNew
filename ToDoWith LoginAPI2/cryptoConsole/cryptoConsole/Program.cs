48f46347fc90c3cedeef74cebb1a0a1362d76df9
Bernhard <b.korn@boservice.de>
2020-09-29 17:39:44 +0200

    cryptoConsole added

---
+++ ToDoWith LoginAPI2/cryptoConsole/cryptoConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptoConsole
+{
+    class Program
+    {
+        private const string  password = "pjoh7nPf1geA3KUo8BSL";
+        private const string salt = "bgIxzFoiIA6HzAZ4XTB2";
+        static void Main(string[] args)
+        {
+            string toencrypt = "Hallo Bernhard";
+            Console.WriteLine($"toencrypt: '{toencrypt}'");
+            string encrypted = CryptographyHelpers.Encrypt( password, salt, toencrypt);
+            string decrypted = CryptographyHelpers.Decrypt(password, salt, encrypted);
+            Console.WriteLine($"decrypted: '{decrypted}'");
+
+
+            Console.WriteLine("Press any key to exit this program");
+            Console.ReadKey();
+        }
+    }
+}
