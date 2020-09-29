0c6f6c2f54cb289d5c3dfb68fa880dd1f5408467
Bernhard <b.korn@boservice.de>
2020-09-29 18:17:05 +0200

    UsersController added

---
+++ ToDoWith LoginAPI2/ToDoWithLoginAPI/ToDoWithLoginAPI/Misc/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoWithLoginAPI.Misc
+{
+    public static class Data
+    {
+        public const string password = "pjoh7nPf1geA3KUo8BSL";
+        public const string salt = "bgIxzFoiIA6HzAZ4XTB2";
+
+    }
+}
---
+++ ToDoWith LoginAPI2/ToDoWithLoginAPI/ToDoWithLoginAPI/Misc/UsersConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoWithLoginAPI.Models;
+
+namespace ToDoWithLoginAPI.Misc
+{
+    public static class UsersConverter
+    {
+        internal static UsersDTO ToDTO(this Users users)
+        {
+            return new UsersDTO()
+            {
+                Id = users.Id,
+                Name = users.Name,
+                EmailAddress = users.EmailAddress,
+                Password = CryptographyHelpers.Encrypt(Data.password, Data.salt, users.PasswordHash)
+
+            };
+
+        }
+        internal static Users FromDTO(this UsersDTO usersDTO)
+        {
+            return new Users()
+            {
+                Id = usersDTO.Id,
+                Name = usersDTO.Name,
+                EmailAddress = usersDTO.EmailAddress,
+                PasswordHash = CryptographyHelpers.Encrypt(Data.password, Data.salt, usersDTO.Password)
+
+            };
+
+        }
+    }
+}
