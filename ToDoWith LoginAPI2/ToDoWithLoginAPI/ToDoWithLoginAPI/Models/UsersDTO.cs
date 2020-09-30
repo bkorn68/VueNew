38519cca3263d1b78953bcb0e41b08fb7354fbae
Bernhard <b.korn@boservice.de>
2020-10-01 01:26:35 +0200

    KendoUIPWA Link

---
+++ ToDoWith LoginAPI2/ToDoWithLoginAPI/ToDoWithLoginAPI/Models/UsersDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoWithLoginAPI.Models
+{
+    public class UsersDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+
+    }
+}
