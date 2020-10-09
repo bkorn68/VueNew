4b309a7fa29015ca6d977f4836df6d5d3022fe5a
Bernhard <b.korn@boservice.de>
2020-10-09 11:09:45 +0200

    Login und Tourstyles added

--- LoginWithVuex/loginwithvuex/src/views/Login.vue
+++ LoginWithVuex/loginwithvuex/src/views/Login.vue
@@ -2,15 +2,16 @@
 <template>
   <div>
     <h2>Login</h2>
-    <div>
-      <label>Benutzername</label>
-      <input type="text" placeholder="Username" v-model="loginname" />
-    </div>
-    <div>
-      <label>Kennwort</label>
-      <input type="text" placeholder="Password" v-model="password" />
-    </div>   
-    <input type="button" @click="login" value="Login" />
+    <div class="container">
+    <label for="uname"><b>Benutzername</b></label>
+    <input type="text" placeholder="Benutzername eingeben" name="loginname" v-model="loginname" required>
+
+    <label for="psw"><b>Kennwort</b></label>
+    <input type="password" placeholder="Kennwort eingeben" name="password" v-model="password" required>
+
+    <button type="submit" @click="login">Login</button>
+    
+  </div>
     <p v-if="msg">{{ msg }}</p>
   </div>
 </template>
@@ -64,3 +65,42 @@ export default {
   }
 };
 </script>
+<style scoped>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+/* Bordered form */
+form {
+  border: 3px solid #f1f1f1;
+}
+/* Full-width inputs */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+/* Set a style for all buttons */
+button {
+  background-color: #031c3d;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+/* Add a hover effect for buttons */
+button:hover {
+  opacity: 0.8;
+}
+/* Add padding to containers */
+.container {
+
+padding-left: 560px;
+  padding-right: 560px;
+vertical-align: middle;
+  
+}
+</style>
--- LoginWithVuex/loginwithvuex/src/views/Tour.vue
+++ LoginWithVuex/loginwithvuex/src/views/Tour.vue
@@ -1,30 +1,23 @@
 <template>
-    <div>
-        <h2>Tour</h2>
-        <div>
-            <label>Monteur</label>
-        <select v-model="selectedSourceID">
+  <div>
+    <h2>Login</h2>
+    <div class="container">
+    <label for="uname"><b>Monteur</b></label>
+    <select v-model="selectedSourceID">
   <option v-for="technichian in technichians" v-bind:value="technichian.SourceID">
-    `${technichian.Name},[${technichian.SourceID}]`
+    {{technichian.Name + '[' + technichian.SourceID + ']' }}
   </option>
 </select>
-<span>SelectedSourceID: {{ selectedSourceID }}</span>     
-</div>
-<div>
-    <label>Tag</label>
+
+    <label for="psw"><b>Tag</b></label>
     <input type="date" v-model="selectedDate" />
-    <span>selectedDate: {{ selectedDate }}</span>
-
-</div>
-<input type="button" @click="getTours" value="getTours" />
 
 
-
-
-
-
-
-    </div>
+    <button type="submit" @click="getTours">Touren</button>
+    
+  </div>
+    
+  </div>
 </template>
 
 <script>
@@ -62,6 +55,52 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
+/* Bordered form */
+form {
+  border: 3px solid #f1f1f1;
+}
+/* Full-width inputs */
+input[type=text], input[type=date] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+/* Full-width select */
+select {
+width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
 
+}
+/* Set a style for all buttons */
+button {
+  background-color: #031c3d;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+/* Add a hover effect for buttons */
+button:hover {
+  opacity: 0.8;
+}
+/* Add padding to containers */
+.container {
+
+padding-left: 560px;
+  padding-right: 560px;
+vertical-align: middle;
+  
+}
 </style>
